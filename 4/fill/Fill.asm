19cfe17d46a8b143a8036a211e565a3fe25a89a4
kikuepi <kikuepi@gmail.com>
2024-05-30 17:57:45 +0900

    CPUまでやりました

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -9,3 +9,74 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+// (INIT)
+// @8192
+// D = A
+// @i
+// M = D
+
+// (LOOP)
+// @i
+// M = M - 1
+// D = M
+// @INIT
+// D;JLT
+// @KBD
+// D = M
+// @WHITE
+// D;JEQ
+// @BLACK
+
+// (BLACK)
+// @SCREEN
+// D = A
+// @i
+// A = D + M
+// M = -1
+// @LOOP
+// 0;JMP
+
+// (WHITE)
+// @SCREEN
+// D = A
+// @i
+// A = D + M
+// M = 0
+// @LOOP
+// 0;JMP
+(INIT)
+@8192
+D = A
+@i
+M = D
+
+(LOOP)
+@i
+M = M - 1
+D = M
+@INIT
+D;JLT
+@KBD
+D = M
+@WHITE
+D;JEQ
+@BLACK
+0;JMP
+
+(BLACK)
+@SCREEN
+D = A
+@i
+A = D + M
+M = -1
+@LOOP
+0;JMP
+
+(WHITE)
+@SCREEN
+D = A
+@i
+A = D + M
+M = 0
+@LOOP
+0;JMP
--- 4/mult/Mult.asm
+++ 4/mult/Mult.asm
@@ -8,3 +8,27 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+@R0
+@R1
+@R2
+M = 0
+@i
+M = 0
+(LOOP)
+    @R1
+    D = M
+    @i
+    D = D - M
+    @END
+    D;JEQ
+    @R0
+    D = M
+    @R2
+    M = M + D
+    @i
+    M = M + 1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
